9d8b80774039cfde7ed2f493348336e287388c7b
darkodimovski <darkodimovski57@gmail.com>
2025-08-20 13:26:03 +0200

    render postgre

--- config/database.ts
+++ config/database.ts
@@ -1,5 +1,3 @@
-
-
 export default ({ env }) => ({
   connection: {
     client: 'postgres',
@@ -9,7 +7,9 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL'),
+      ssl: env.bool('DATABASE_SSL') ? {
+        rejectUnauthorized: false
+      } : false,
       schema: env('DATABASE_SCHEMA', 'public'),
     },
     pool: { min: 2, max: 10 },
