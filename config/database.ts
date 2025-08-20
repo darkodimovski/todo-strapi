3bc63170996735baec4837c8442a9fd644d01a66
darkodimovski <darkodimovski57@gmail.com>
2025-08-20 13:15:11 +0200

    Switch to postgreSQL

--- config/database.ts
+++ config/database.ts
@@ -1,34 +1,18 @@
-import path from 'path';
 
-export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
 
-  const connections = {
-    postgres: {
-      connection: {
-        host: env('DATABASE_HOST', 'db.zbfrdficnzkpbsmgvnqv.supabase.co'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-postgreSQL'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-        ssl: { rejectUnauthorized: false },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
-  };
-};
+    pool: { min: 2, max: 10 },
+    acquireConnectionTimeout: 60000,
+  },
+});
